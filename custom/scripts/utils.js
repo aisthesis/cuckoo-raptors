206fc8b025978dff52a85b80b73131073cbe17ed
Marshall Farrier <info@marshallfarrier.com>
2013-10-28 18:08:15 -0700

    Random nest setup

--- custom/scripts/utils.js
+++ custom/scripts/utils.js
@@ -2,49 +2,26 @@ var codeMelon = codeMelon || {};
 codeMelon.utils = codeMelon.utils || {};
 
 /**
- * Return an array consisting of distinct random numbers.
- * @param choices Required parameter specifying the number of distinct
- * choices available. If only this parameter is given, an array will be 
- * returned of this size where the entries are distinct random
- * integers in the range [0, choices - 1]
- * @param start The lowest value in the range of random numbers
- * to be returned. Defaults to 0. 
- * @param choose The size of the array returned. Must be between
- * 1 and choices. Defaults to choices.
- * Examples:
- * codeMelon.utils.randomize( {choices: 5} );
- * might return [3, 0, 1, 4, 2]
- * codeMelon.utils.randomize( {choices: 4, start: 2} );
- * might return [3, 2, 5, 4]
- * codeMelon.utils.randomize( {choices: 7, choose: 3} );
- * might return [4, 0, 6]
+ * Without the optional <code>choose</code> parameter, the function simply
+ * returns a randomly shuffled copy of the input array.
+ * With the <code>choose</code> parameter, an array of 
+ * length <code>choose</code> is returned who values
+ * are randomly chosen from the values in the input array.
+ * If the values in the input array are distinct, the values
+ * in the output will be distinct. If the input array
+ * contains duplicates, the output may contain duplicates.
+ * WARNING: This method will shuffle the values in the input
+ * array, so use a copy if this array needs to be preserved.
  */
-
-codeMelon.utils.randomize = function(params) {
-    "use strict";
-    var choices = params.choices,
-        start = params.start || 0,
-        choose = params.choose || choices,
-        arr = [],
-        result = [],
-        max = start + choices,
-        i, 
-        j, 
-        tmp;
-
-    if (choose > choices) {
-        throw new RangeError("cannot choose more values than available choices");
-    }
-
-    // initialize array to be randomized
-    for (i = start; i < max; i++) {
-        arr.push(i);
-    }
+codeMelon.utils.randomize = function(arr, choose) {
+    var result = [],
+        resultSize = choose || arr.length,
+        i, j, tmp;
 
     // randomize the first 'choose' values
-    for (i = 0; i < choose; i++) {
+    for (i = 0; i < resultSize; i++) {
         // find value with which to swap i
-        j = Math.floor(Math.random() * (choices - i)) + i;
+        j = Math.floor(Math.random() * (arr.length - i)) + i;
         // make the swap
         tmp = arr[i];
         arr[i] = arr[j];
--- custom/scripts/views/app-view.js
+++ custom/scripts/views/app-view.js
@@ -2,24 +2,123 @@ var codeMelon = codeMelon || {};
 codeMelon.games = codeMelon.games || {}; 
 
 codeMelon.games.AppView = Backbone.View.extend({
-    initialize: function() {
+    initialize: function(options) {
         var _this = this;
 
         _.bindAll(_this,
-            'render'
+            'render',
+            'setConstants',
+            'getNest',
+            'getForeignEggCount',
+            'drawCells',
+            'drawCell',
+            'drawUniformCells',
+            'drawForeignCells'
         );
 
+        _this.setConstants(options);
         _this.render();
     },
 
     render: function() {
-        var _this = this,
-            context = _this.el.getContext('2d');
+        var _this = this;
 
-        context.font = '38pt Arial';
-        context.fillStyle = 'cornflowerblue';
-        context.strokeStyle = 'blue';
-        context.fillText('Hello Canvas', _this.el.width / 2 - 128, _this.el.height / 2 + 15);
-        context.strokeText('Hello Canvas', _this.el.width / 2 - 128, _this.el.height / 2 + 15);
+        _this.drawUniformCells();
+        _this.drawForeignCells();
+    },
+
+    setConstants: function(options) {
+        var _this = this;
+
+        _this.CONTEXT = _this.el.getContext('2d');
+        _this.CANVAS_PADDING = 16;
+        _this.SIDE_CELLS = 4;
+        _this.SIDE_SIZE = (_this.el.width - 2 * _this.CANVAS_PADDING) / _this.SIDE_CELLS,
+        _this.FOREIGN_EGG_COUNT = _this.getForeignEggCount();
+        _this.NEST = _this.getNest();
+        _this.NATIVE_CELL_FILL_STYLE = '#999999';
+        _this.FOREIGN_CELL_FILL_STYLE = '#FF0000';
+        _this.CELL_STROKE_STYLE = '#cccccc';
+        _this.CELL_BORDER_WIDTH = 16;
+    },
+
+    /**
+     * NEST is an array of size SIDE_CELLS * SIDE_CELLS representing
+     * a square grid. 
+     * Cells with no eggs (on the corners) have a value of -1.
+     * Cells with native eggs have a value of 0.
+     * Cells with foreign eggs have a value of 1.
+     */
+    getNest: function() {
+        var _this = this,
+            result = [
+                -1, 0, 0, -1,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+                -1, 0, 0, -1],
+            valuesToRandomize = [],
+            foreignEggs,
+            i;
+
+        // get cells occupied by eggs
+        for (i = 0; i < result.length; i++) {
+            if (result[i] === 0) {
+                valuesToRandomize.push(i);
+            }
+        }
+        // choose foreign egg cells randomly among the cells occupied by eggs
+        foreignEggs = codeMelon.utils.randomize(valuesToRandomize, _this.FOREIGN_EGG_COUNT);
+        for (i = 0; i < foreignEggs.length; i++) {
+            result[foreignEggs[i]] = 1;
+        }
+        return result;
+    },
+
+    getForeignEggCount: function() {
+        // TODO link to context
+        return 3;
+    },
+
+    /**
+     * @param minTypeToFill 0 to fill all cells with the given
+     * style, 1 to fill only foreign cells with the given style
+     */
+    drawCells: function(fillStyle, minTypeToFill) {
+        var _this = this;
+
+        _this.CONTEXT.save();
+        _this.CONTEXT.fillStyle = fillStyle;
+        _this.CONTEXT.strokeStyle = _this.CELL_STROKE_STYLE;
+        _this.CONTEXT.lineWidth = _this.CELL_BORDER_WIDTH;
+        for (var i = 0; i < _this.NEST.length; i++) {
+            if (_this.NEST[i] >= minTypeToFill) {
+                _this.drawCell(i);
+            }
+        }
+        _this.CONTEXT.restore();
+    },
+
+    drawCell: function(i) {
+        var _this = this,
+            row = Math.floor(i / _this.SIDE_CELLS),
+            col = i % _this.SIDE_CELLS,
+            yTop = _this.CANVAS_PADDING + row * _this.SIDE_SIZE,
+            xLeft = _this.CANVAS_PADDING + col * _this.SIDE_SIZE;
+
+        _this.CONTEXT.fillRect(xLeft, yTop, _this.SIDE_SIZE, _this.SIDE_SIZE);
+        _this.CONTEXT.rect(xLeft, yTop, _this.SIDE_SIZE, _this.SIDE_SIZE);
+        _this.CONTEXT.stroke();
+    },
+
+    drawUniformCells: function() {
+        var _this = this;
+
+        _this.drawCells(_this.NATIVE_CELL_FILL_STYLE, 0);
+    },
+
+    drawForeignCells: function() {
+        var _this = this;
+
+        _this.drawCells(_this.FOREIGN_CELL_FILL_STYLE, 1);
     }
 });
