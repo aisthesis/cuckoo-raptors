52065840f38ae3d239bc1c92e6f393bc1940c04c
Marshall Farrier <info@marshallfarrier.com>
2013-11-14 00:38:38 -0800

    Namespacing.

--- scripts/app-init.js
+++ scripts/app-init.js
@@ -1,3 +1,8 @@
-var appView = new _cg.AppView({
+var app = app || {};
+
+(function(_cg) {
+    "use strict";
+    app.appView = new _cg.AppView({
         el: $('#game-content')
-    }); 
+    });
+})(codeMelon.games);
